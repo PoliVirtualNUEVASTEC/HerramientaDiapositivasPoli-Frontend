7631da42fa19a8e05d15ff0453ea387759a93dfe
MejiaMigue <miguel_mejia82201@elpoli.edu.co>
2026-03-17 15:30:29 -0500

    Dashboard page, PdfUploader, TextUpoloader, presentationServices

---
+++ src/services/presentationService.js
@@ -0,0 +1,24 @@
+import api from '../api/axios'
+
+// PDF
+export const uploadPDF = async (file) => {
+  const formData = new FormData()
+  formData.append('file', file)
+
+  const res = await api.post('/api/presentations/pdf', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  })
+
+  return res.data
+}
+
+// TEXTO
+export const sendText = async (text) => {
+  const res = await api.post('/api/presentations/text', {
+    text
+  })
+
+  return res.data
+}
