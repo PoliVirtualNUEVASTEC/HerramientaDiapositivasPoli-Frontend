374c20867bfd5bcc88f314a283d87d6dd1013d21
MejiaMigue <miguel_mejia82201@elpoli.edu.co>
2026-03-25 11:59:15 -0500

    feature: VisualizationList and VisualizationPage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/Register';
 import ResetPassword from './pages/ResetPassword';
+import PresentationPreview from './pages/PresentationPreview';
 import ProtectedRoute from './router/ProtectedRoute';
 import SesionRoute from './router/SesionRoute';
 
@@ -18,47 +19,12 @@ function App() {
         <AuthProvider>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
-              path="/login"
-              element={
-                <SesionRoute>
-                  <Login />
-                </SesionRoute>
-              }
-            />
-            <Route
-              path="/forgot-password"
-              element={
-                <SesionRoute>
-                  <ForgotPassword />
-                </SesionRoute>
-              }
-            />
-            <Route
-              path="/reset-password"
-              element={
-                <SesionRoute>
-                  <ResetPassword />
-                </SesionRoute>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <SesionRoute>
-                  <Register />
-                </SesionRoute>
-              }
-            />
-
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
+            <Route path="/login" element={<SesionRoute><Login /></SesionRoute>} />
+            <Route path="/forgot-password" element={<SesionRoute><ForgotPassword /></SesionRoute>} />
+            <Route path="/reset-password" element={<SesionRoute><ResetPassword /></SesionRoute>} />
+            <Route path="/register" element={<SesionRoute><Register /></SesionRoute>} />
+            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+            <Route path="/preview/:id" element={<ProtectedRoute><PresentationPreview /></ProtectedRoute>} />
           </Routes>
         </AuthProvider>
       </BrowserRouter>
@@ -66,4 +32,4 @@ function App() {
   );
 }
 
-export default App;
+ export default App;
